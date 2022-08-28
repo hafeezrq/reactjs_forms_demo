8765293344698493d68321d56c64a711fc246273
hafeezrq <hafeezrq@yahoo.com>
2022-08-28 17:53:48 +1000

    Add Form, Input, and Sematic ui.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 export class SearchBar extends Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className='ui segment'>
+        <form className='ui form'>
+          <div className='field'>
+            <label htmlFor=''>Image Search</label>
+            <input type='text' />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
