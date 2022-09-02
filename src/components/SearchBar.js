5c6f1ebb92ee7ea3f6282de5c184b47b99465692
hafeezrq <hafeezrq@yahoo.com>
2022-09-02 15:50:42 +1000

    refactor App, props from child to parent by invoking callback.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,7 +8,7 @@ export class SearchBar extends Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
