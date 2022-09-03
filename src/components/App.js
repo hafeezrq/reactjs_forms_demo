b646dec3171988e3fe4c420a937dd83926ef7653
hafeezrq <hafeezrq@yahoo.com>
2022-09-03 17:52:42 +1000

    Refactor to solve the 'this' issue in App.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+  onSearchSubmit = async term => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
@@ -13,11 +14,13 @@ class App extends React.Component {
       },
     });
     console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {this.state.images.length} images
       </div>
     );
   }
