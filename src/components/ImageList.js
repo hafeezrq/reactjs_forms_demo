de9d5e12059ea6115f646a2bba76170497a37bcd
hafeezrq <hafeezrq@yahoo.com>
2022-09-04 15:52:31 +1000

    Fetch data and display in a list.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const ImageList = () => {
-  return <div>ImageList</div>;
+const ImageList = props => {
+  const images = props.images.map(image => (
+    <img src={image.urls.regular} alt={image.description} key={image.id} />
+  ));
+  return <div>{images}</div>;
 };
 
 export default ImageList;
