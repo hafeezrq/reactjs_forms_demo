e27e0be040725941f33aba897156b200562a17fb
hafeezrq <hafeezrq@yahoo.com>
2022-09-09 14:47:18 +1000

    Access DOM using React Ref and improve display.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
   render() {
     const { description, urls } = this.props.image;
+    console.log(this.state.spans);
+
     return (
-      <div>
-        <img src={urls.regular} alt={description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img src={urls.regular} alt={description} ref={this.imageRef} />
       </div>
     );
   }
